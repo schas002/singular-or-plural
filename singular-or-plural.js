f855b5b4c131eb4cf95db00302d7038dc049ff2e
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2017-05-10 13:34:48 +0400

    add the code

---
+++ singular-or-plural.js
@@ -0,0 +1,9 @@
+function singularOrPlural(count, singular, plural) {
+  if(count != count) {
+    return null;
+  }
+  value = Math.abs(count);
+  return value % 10 == 1 && value % 100 != 11 ? singular : plural;
+}
+
+module.exports = singularOrPlural;
